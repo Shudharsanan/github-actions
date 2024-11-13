03d48a8f66540bc089b4e090ecc41e8459dc8180
Shudharsanan Muthuraj <shudharsananm.1989@gmail.com>
2024-11-13 18:42:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -21,19 +21,24 @@ credentials = ClientSecretCredential(
 
 blob_service_client = BlobServiceClient(account_url="https://mlwdp100labs7488979237.blob.core.windows.net", credential=credentials)
 
-ws = Workspace(
-              subscription_id = subscription_id,
-              resource_group = resource_group,
-              workspace_name = workspace_name,
-              auth = credentials)
+containers = blob_service_client.list_containers()
+for container in containers:
+    print(container.name)
 
-compute_target = ComputeTarget(worspace = ws, name = 'trial-compute')
 
-run = experiment.submit(script = 'Users/shudharsananm.1989/my_own_code/github_actions_testing.py', compute_target = compute_target)
+#ws = Workspace(
+#              subscription_id = subscription_id,
+#              resource_group = resource_group,
+#              workspace_name = workspace_name,
+#              auth = credentials)
 
-if run.status == 'completed':
-  print("Run completed")
-elif run.status == 'Failed':
-  print(f"Run failed with error: {run.error}")
-else:
-  print(f'Run was {run.status}')
+#compute_target = ComputeTarget(worspace = ws, name = 'trial-compute')
+
+#run = experiment.submit(script = 'Users/shudharsananm.1989/my_own_code/github_actions_testing.py', compute_target = compute_target)
+
+#if run.status == 'completed':
+#  print("Run completed")
+#elif run.status == 'Failed':
+#  print(f"Run failed with error: {run.error}")
+#else:
+#  print(f'Run was {run.status}')
