1b1fda992bec7f97453408f6fe8f92259c3a7a8e
Shudharsanan Muthuraj <shudharsananm.1989@gmail.com>
2024-11-13 18:55:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -29,17 +29,15 @@ ws = Workspace(
               subscription_id = subscription_id,
               resource_group = resource_group,
               workspace_name = workspace_name)
-#              auth = credentials)
+
 
 compute_target = ComputeTarget(workspace = ws, name = 'trial-compute')
 
+run = experiment.submit(script = 'Users/shudharsananm.1989/my_own_code/github_actions_testing.py', compute_target = compute_target)
 
-
-#run = experiment.submit(script = 'Users/shudharsananm.1989/my_own_code/github_actions_testing.py', compute_target = compute_target)
-
-#if run.status == 'completed':
-#  print("Run completed")
-#elif run.status == 'Failed':
-#  print(f"Run failed with error: {run.error}")
-#else:
-#  print(f'Run was {run.status}')
+if run.status == 'completed':
+  print("Run completed")
+elif run.status == 'Failed':
+  print(f"Run failed with error: {run.error}")
+else:
+  print(f'Run was {run.status}')
