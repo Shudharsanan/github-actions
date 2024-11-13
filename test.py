b015c532c8afb86f4cd89249d69bb8494d6252ad
Shudharsanan Muthuraj <shudharsananm.1989@gmail.com>
2024-11-13 14:54:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,14 @@ from azureml.core.compute import ComputeTarget
 from azureml.core import Experiment
 from azureml.core.run import Run
 
+
+tenant_id = "ee10b4ad-568d-4026-9cca-3f47d86d0d04"
+client_id = "c8533615-50a1-4b7b-87a3-0e1655b2c357"
+client_secret = "QxC8Q~b_m1EYYfKMuSkRRjhzp-5L9ZlEJTjhZdsc"
+subscription_id = "89932198-e74d-4f28-b227-95f2729192c0"
+resource_group = "rg-dp100-labs"
+workspace_name = "mlw-dp100-labs"
+
 credentials = ClientSecretCredential(
               tenant_id = tenant_id,
               client_id = client_id,
